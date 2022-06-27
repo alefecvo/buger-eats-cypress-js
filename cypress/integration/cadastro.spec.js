71c9deb955a7b41e9265ad4307361aa662cb6203
Álefe Cardozo Silva <alefecardozo@gmail.com>
2022-06-27 17:38:23 -0300

    create news tests

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -8,50 +8,50 @@ describe('Cadastro', () => {
     });
 
     //Test
-    it('TEST 1 - FLUXO PRINCIPAL - Cadastrar novo entregador', ()=>{
+    it('TEST 1 - FLUXO PRINCIPAL - Cadastrar novo entregador com cpf correto', ()=>{
         //Acessando cadastro de entregador
         cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
     
         //Criando massa de dado para entregador preencher formulário
-        var dadosEntregador ={
-            nome: 'Júlia Marrone',
+        var delivery ={
+            name: 'Júlia Marrone',
             cpf: '12345678910',
             email: 'juliamarrone@bol.com',
             whatsapp: '11912121212',
-            endereco:{
-                cep: '35794106',
-                rua: 'Rua Joinvile',
-                numero: '130',
-                complemento: 'casa',
-                bairro: 'Jardim Paraíso',
-                cidade_uf: 'Curvelo/MG'
+            address:{
+                postalcode: '35794106',
+                street: 'Rua Joinvile',
+                number: '130',
+                details: 'casa',
+                district: 'Jardim Paraíso',
+                city_state: 'Curvelo/MG'
             },
-            metodo_entrega: 'Moto',
+            delivery_method: 'Moto',
             cnh: 'cnh-digital.jpg'
             
     }
         //Preenchimento dados do entregador
-        cy.get('#page-deliver input[name=name]').type(dadosEntregador.nome);
-        cy.get('#page-deliver input[name=cpf]').type(dadosEntregador.cpf);
-        cy.get('#page-deliver input[name=email]').type(dadosEntregador.email);
-        cy.get('#page-deliver input[name=whatsapp]').type(dadosEntregador.whatsapp);
+        cy.get('#page-deliver input[name=name]').type(delivery.name);
+        cy.get('#page-deliver input[name=cpf]').type(delivery.cpf);
+        cy.get('#page-deliver input[name=email]').type(delivery.email);
+        cy.get('#page-deliver input[name=whatsapp]').type(delivery.whatsapp);
 
         //Preenchimento dados de endereço
-        cy.get('#page-deliver input[name=postalcode]').type(dadosEntregador.endereco.cep);
+        cy.get('#page-deliver input[name=postalcode]').type(delivery.address.postalcode);
         cy.get('#page-deliver input[value="Buscar CEP"]').click();
-        cy.get('#page-deliver input[name="address-number"]').type(dadosEntregador.endereco.numero);
-        cy.get('#page-deliver input[name="address-details"]').type(dadosEntregador.endereco.complemento);
+        cy.get('#page-deliver input[name="address-number"]').type(delivery.address.number);
+        cy.get('#page-deliver input[name="address-details"]').type(delivery.address.details);
 
         //Validando preenchimento campos de endereço
-        cy.get('input[name="address"]').should('have.value',dadosEntregador.endereco.rua);
-        cy.get('input[name="district"]').should('have.value',dadosEntregador.endereco.bairro);
-        cy.get('input[name="city-uf"]').should('have.value',dadosEntregador.endereco.cidade_uf);
+        cy.get('input[name="address"]').should('have.value',delivery.address.street);
+        cy.get('input[name="district"]').should('have.value',delivery.address.district);
+        cy.get('input[name="city-uf"]').should('have.value',delivery.address.city_state);
 
         //Preenchendo dados de método de entrega por Moto
-        cy.contains('.delivery-method li', dadosEntregador.metodo_entrega).click();
+        cy.contains('.delivery-method li', delivery.delivery_method).click();
         
         //Fazendo upload da CNH
-        cy.get('input[accept^="image"]').attachFile('/images/' + dadosEntregador.cnh);
+        cy.get('input[accept^="image"]').attachFile('/images/' + delivery.cnh);
 
         //Submetendo dados do cadastro
         cy.get('button[type="submit"]').click();
@@ -60,5 +60,60 @@ describe('Cadastro', () => {
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
         
         cy.get('.swal2-container .swal2-html-container').should('have.text',expectedMessage);
+    })
+
+    //Test
+    it('TEST 2 - FLUXO ALTERNATIVO - Cadastrar novo entregador com cpf incorreto', ()=>{
+        //Acessando cadastro de entregador
+        cy.get('#page-deliver form h1').should('have.text','Cadastre-se para  fazer entregas')
+    
+        //Criando massa de dado para entregador preencher formulário
+        var delivery ={
+            name: 'Júlia Marrone',
+            cpf: '123456789',
+            email: 'juliamarrone@bol.com',
+            whatsapp: '11912121212',
+            address:{
+                postalcode: '35794106',
+                street: 'Rua Joinvile',
+                number: '130',
+                details: 'casa',
+                district: 'Jardim Paraíso',
+                city_state: 'Curvelo/MG'
+            },
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg'
+            
+    }
+        //Preenchimento dados do entregador
+        cy.get('#page-deliver input[name=name]').type(delivery.name);
+        cy.get('#page-deliver input[name=cpf]').type(delivery.cpf);
+        cy.get('#page-deliver input[name=email]').type(delivery.email);
+        cy.get('#page-deliver input[name=whatsapp]').type(delivery.whatsapp);
+
+        //Preenchimento dados de endereço
+        cy.get('#page-deliver input[name=postalcode]').type(delivery.address.postalcode);
+        cy.get('#page-deliver input[value="Buscar CEP"]').click();
+        cy.get('#page-deliver input[name="address-number"]').type(delivery.address.number);
+        cy.get('#page-deliver input[name="address-details"]').type(delivery.address.details);
+
+        //Validando preenchimento campos de endereço
+        cy.get('input[name="address"]').should('have.value',delivery.address.street);
+        cy.get('input[name="district"]').should('have.value',delivery.address.district);
+        cy.get('input[name="city-uf"]').should('have.value',delivery.address.city_state);
+
+        //Preenchendo dados de método de entrega por Moto
+        cy.contains('.delivery-method li', delivery.delivery_method).click();
+        
+        //Fazendo upload da CNH
+        cy.get('input[accept^="image"]').attachFile('/images/' + delivery.cnh);
+
+        //Submetendo dados do cadastro
+        cy.get('button[type="submit"]').click();
+
+        //Validando cadastro do entregador
+        const expectedMessage = 'Oops! CPF inválido'
+        
+        cy.get('.alert-error').should('have.text',expectedMessage);
     })
 });
